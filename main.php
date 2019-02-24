21bc0101860c2a22157e9b49bc0a1c8c71b46da3
Kelvin Spencer <kelvin@spencerfirmllc.com>
2019-02-24 10:13:29 -0600

    Update main.php

--- main.php
+++ main.php
@@ -13,13 +13,16 @@ print "...\n";
 //Fetch universe client id and client secret from keystore
 //$universe_cred_url = 'https://api.apify.com/v2/key-value-stores?token='+$_ENV[APIFY_TOKEN]+'&name=my-store-name';
 //$url = 'https://api.apify.com/v2/key-value-stores/storeId/records/recordKey?disableRedirect=&token='.$_ENV[APIFY_TOKEN];
-$url ='https://api.apify.com/v2/key-value-stores?token='.$_ENV[APIFY_TOKEN].'&offset=10&limit=99&desc=true&unnamed=true';
-$data = getUrl($url);
-print_r( $data );
+//$url ='https://api.apify.com/v2/key-value-stores?token='.$_ENV[APIFY_TOKEN].'&offset=10&limit=99&desc=true&unnamed=true';
+//$data = getUrl($url);
+//print_r( $data );
 
 //Save to keystore
 //https://api.apify.com/v2/key-value-stores?token=eqMr5hwRGiyFuhZgiRwTyppos&name=my-store-name
-
+$data['$storeID'] = '';
+$data['recordKey'] = '';
+$data['token'] = $_ENV[APIFY_TOKEN];
+saveKey($data);
 
 function getUrl($url){
   $ch = curl_init();
@@ -33,6 +36,49 @@ function getUrl($url){
 
   //var_dump($response);
   return $response;
+}
+
+
+function createStore($keyname){
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, "https://api.apify.com/v2/key-value-stores?name=".$keyname);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_HEADER, FALSE);
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+
+  $response = curl_exec($ch);
+  curl_close($ch);
+
+  //var_dump($response);
+}
+
+
+function saveKey($data){
+  $ch = curl_init();
+  $storeID = $data['$storeID'];
+  $recordKey = $data['recordKey'];
+  $token = $data['token'];
+  
+  $keyname = 'foo';
+  $keyvalue = 'bar';
+  
+  curl_setopt($ch, CURLOPT_URL, "https://api.apify.com/v2/key-value-stores/".$storeID."/records/".$recordKey."?token=".$token);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+
+  curl_setopt($ch, CURLOPT_POSTFIELDS, "{
+    \"".$keyname."\": \"".$keyvalue."\"
+  }");
+
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json"
+  ));
+
+  $response = curl_exec($ch);
+  curl_close($ch);
 }
 
 // print "Fetching http://example.com ...\n";
