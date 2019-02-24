4bcfdf36aaece0f57cc2bb1fb3bf97e98cfbe94b
Kelvin Spencer <kelvin@spencerfirmllc.com>
2019-02-24 09:45:21 -0600

    Update main.php

--- main.php
+++ main.php
@@ -6,15 +6,16 @@ print "Starting ...\n";
 print "ENV vars:\n";
 
 print $_ENV[APIFY_CONTAINER_URL];
+print "...\n";
 
-//print_r($_ENV);
+print_r($_ENV);
 
-print "Fetching http://example.com ...\n";
-$exampleComHtml = file_get_contents('http://example.com');
-print "Searching for <h1> tag contents ...\n";
-preg_match_all('/<h1>(.*?)<\/h1>/', $exampleComHtml, $matches);
-print "Found: " . $matches[1][0] . "\n";
-print "I am done!\n";
+// print "Fetching http://example.com ...\n";
+// $exampleComHtml = file_get_contents('http://example.com');
+// print "Searching for <h1> tag contents ...\n";
+// preg_match_all('/<h1>(.*?)<\/h1>/', $exampleComHtml, $matches);
+// print "Found: " . $matches[1][0] . "\n";
+// print "I am done!\n";
 
 
 
